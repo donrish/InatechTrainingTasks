d901e91b6b6ea300450cda52a27300a7d2b6185f
Rishab <113574308+donrish@users.noreply.github.com>
2023-01-25 19:52:55 +0530

    Add files via upload

---
+++ task2/Tvchannelpack2c.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tasks.task2
+{
+    internal class Tvchannelpack2c
+    {
+        public static void Main()
+        {
+            string package;
+            Console.WriteLine("Package A contains: South special,Children's club,Movies");
+            Console.WriteLine("Package B contains: News,sports,movies,regional");
+            Console.WriteLine("Package C contains: Discovery,history,national");
+            Console.WriteLine("Enter your required package:");
+            package = Console.ReadLine();
+            switch (package)
+            {
+                case "A":
+                    Console.WriteLine("Rs 250");
+                    break;
+                case "B":
+                    Console.WriteLine("Rs 450");
+                    break;
+                case "C":
+                    Console.WriteLine("Rs 350");
+                    break;
+                default:
+                    Console.WriteLine("Enter the package you need");
+                    break;
+            }
+        }
+    }
+}
---
+++ task6/employ.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Tasks
+{
+    internal class employ
+    {
+        public int Empid { get; set; }
+        public string Empname { get; set; }
+        public string Empgender { get; set; }
+        public int Empage { get; set; }
+        public void readfEmp()
+        {
+            FileStream emp = new FileStream("D:\\INATECH\\inatech app\\mango.txt", FileMode.OpenOrCreate, FileAccess.Read);
+            StreamReader sr = new StreamReader(emp);
+            sr.BaseStream.Seek(0, SeekOrigin.Begin);
+            string str = sr.ReadToEnd();
+            Console.WriteLine(str);
+        }
+        public static void Main(string[] args)
+        {
+            employ emp = new employ();
+            Console.WriteLine("Enter the employee name:");
+            emp.Empname = Console.ReadLine();
+            Console.WriteLine("Enter the employee gender:");
+            emp.Empgender = Console.ReadLine();
+            Console.WriteLine("Enter the employee ID:");
+            emp.Empid = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the employee age:");
+            emp.Empage = Convert.ToInt32(Console.ReadLine());
+
+            FileStream Emp = new FileStream("D:\\INATECH\\inatech app\\mango.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(Emp);
+            sw.Write(emp.Empname);
+            sw.Write(emp.Empgender);
+            sw.Write(emp.Empage);
+            sw.Write(emp.Empid);
+            sw.Flush();
+            sw.Close();
+            Emp.Close();
+            emp.readfEmp();
+        }
+    }
+}
